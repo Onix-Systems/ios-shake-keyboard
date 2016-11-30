b525e294c3b2e913b4606d2cda618a30bef3c238
Artem Chaykovsky <artem.chaykovsky@onix-systems.com>
2016-11-30 18:41:39 +0200

    Add initial project (close #4)

---
+++ 8-ball keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8-ball keyboard
+//
+//  Created by Artem Chaykovsky on 11/30/16.
+//  Copyright © 2016 Onix-Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
