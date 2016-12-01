ad994ec8df586cb6848489e022d113c32d81aa96
Artem Chaykovsky <artem.chaykovsky@onix-systems.com>
2016-12-01 16:34:16 +0200

    Add "shake to answer" functionality to keyboard extension (close #8)

--- 8-ball keyboard/ViewController.swift
+++ 8-ball keyboard/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,5 +20,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
--- Eight Ball Keyboard/KeyboardViewController.swift
+++ Eight Ball Keyboard/KeyboardViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+// swiftlint:disable implicit_getter
+
+final class CustomView: UIView {
+    override var canBecomeFirstResponder: Bool {
+        get { return true }
+    }
+}
+
 class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
@@ -15,25 +23,10 @@ class KeyboardViewController: UIInputViewController {
     override func updateViewConstraints() {
         super.updateViewConstraints()
 
-        // Add custom view sizing constraints here
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-
-        self.view.addSubview(self.nextKeyboardButton)
-
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,15 +40,16 @@ class KeyboardViewController: UIInputViewController {
 
     override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
-
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.becomeFirstResponder()
+    }
+
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.motionShake {
+            self.textDocumentProxy.insertText("42")
+        }
+    }
 }
